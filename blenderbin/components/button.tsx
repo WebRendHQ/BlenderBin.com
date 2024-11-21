ab72053a32ccad5a5befa8263f2c60cb807d1a03
gitkevinvera <ceo@webrend.com>
2024-11-21 14:15:28 -0500

    added addon template and dashboard template

---
+++ blenderbin/components/button.tsx
@@ -0,0 +1,19 @@
+// components/Button.tsx
+type ButtonProps = {
+    label: string;
+    onClick: () => void;
+  };
+  
+  const Button = ({ label, onClick }: ButtonProps) => {
+    return (
+      <button
+        onClick={onClick}
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+      >
+        {label}
+      </button>
+    );
+  };
+  
+  export default Button;
+  
