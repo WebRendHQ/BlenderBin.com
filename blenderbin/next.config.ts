5b29801c9b3b3911fd33f2529509905d9378f9ef
gitkevinvera <ceo@webrend.com>
2024-11-21 17:18:01 -0500

    ignoring functions folder

--- blenderbin/next.config.ts
+++ blenderbin/next.config.ts
@@ -1,23 +1,7 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
-  webpack: (config) => {
-    return {
-      ...config,
-      watchOptions: {
-        ...config.watchOptions,
-        ignored: [
-          // Ensure we have a default array if config.watchOptions?.ignored doesn't exist
-          ...(Array.isArray(config.watchOptions?.ignored) 
-            ? config.watchOptions.ignored 
-            : ['/node_modules/']),
-          '**/functions/**'
-        ]
-      }
-    }
-  }
-}
+  /* config options here */
+};
 
-export default nextConfig
+export default nextConfig;
