a587435a992b7a09d1d7cc023f38aa0fca61136c
gitkevinvera <ceo@webrend.com>
2024-11-21 17:21:19 -0500

    Update next.config.ts

--- blenderbin/next.config.ts
+++ blenderbin/next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'export',
+  // If you're deploying to GitHub Pages, you might need:
+  basePath: '/BlenderBin.com',
+  images: {
+    unoptimized: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
